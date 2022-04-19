14af2ce2e4a8f39776e8814207f0d3da46dd80ad
Moisés A. C. Tulitekule <moisestulitekule7@gmail.com>
2022-04-19 21:25:01 +0100

    alterar

--- app.js
+++ app.js
@@ -3,22 +3,24 @@
 
 function sals(t, l, c, s, z, id){
     var sal = document.getElementsByClassName(id);
-    sal[0].style.top = t;
-    sal[0].style.left = l;
-    sal[0].style.color = c;
-    sal[0].style.fontSize = s;
-    sal[0].style.zindex = z;
+   sal[0].style.top = t;
+   sal[0].style.left = l;
+   sal[0].style.color = c;
+   sal[0].style.fontSize = s;
+   sal[0].style.zindex = z;
 
 }
-sals('5%', '50%', '#1b95cd', '250px', '20', "GB");
-sals('1%', '10%', 'red', '150px', '19', "ES");
-sals('15%', '30%', 'blue', '100px', '18', "FR");
-sals('35%', '60%', 'green', '170px', '17', "IT");
-sals('54%', '20%', 'orange', '170px', '16', "GE");
-sals('54%', '35%', 'yellow', '100px', '15', "JP");
-sals('10%', '55%', 'grey', '80px', '14', "RU");
-sals('27%', '21%', 'lightgrey', '100px', '13', "KO");
-sals('37%', '31%', '#333', '65px', '12', "PO");
-sals('67%', '51%', 'pink', '125px', '11', "HI");
-sals('47%', '11%', 'lightblue', '115px', '10', "HE");
-sals('27%', '31%', 'lightgreen', '185px', '9', "SW");
+
+sals('10%','50%','CRIMSON','200px', '20','HL');
+sals('5%','20%','blue','100px','12','TN');
+sals('20%','10%','GREY','180PX', '30','TL');
+sals('21%','30%','GREEN','120PX', '10','AT');
+sals('18%','19%','PURPLE','180PX', '15','DV');
+sals('8%','30%','BLACK','115PX', '19','UT');
+sals('25%','12%','BLUELIGTH','180PX', '21','ND');
+sals('50%','25%','CRIMSON','185px','32','MR');
+sals('5%','60%','YELLOW','150px','30','MS');
+sals('18%','19%','GREEN','115PX', '10','SL');
+sals('38%','11%','RED','170PX', '15','EL');
+sals('31%','50%','BLACK','190PX', '10','CT');
+  
